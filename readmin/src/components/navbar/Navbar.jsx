5aef1dd5f08f395be50e63e4ce474d28aaf3ccdc
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2022-10-10 19:31:41 -0300

    Navbar searchbox and language
    Added two icons for search and language selection.

--- readmin/src/components/navbar/Navbar.jsx
+++ readmin/src/components/navbar/Navbar.jsx
@@ -1,8 +1,23 @@
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
 import "./navbar.scss"
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+        <div className="wrapper">
+            <div className="search">
+                <input type="text" placeholder="Search..." />
+                    <SearchOutlinedIcon/>
+            </div>
+            <div className="items">
+                <div className="item">
+                    <LanguageOutlinedIcon/>
+                    English
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
