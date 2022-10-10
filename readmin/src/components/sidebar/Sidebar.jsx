158ba0704128bbc61af215c258c2a95ae68775d4
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2022-10-10 19:03:55 -0300

    Sidebar itens and icons
    Created the itens and icons on the sidebar.
    The icons are from Material UI.

--- readmin/src/components/sidebar/Sidebar.jsx
+++ readmin/src/components/sidebar/Sidebar.jsx
@@ -1,3 +1,15 @@
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import StoreOutlinedIcon from '@mui/icons-material/StoreOutlined';
+import CreditCardOutlinedIcon from '@mui/icons-material/CreditCardOutlined';
+import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
+import InsertChartOutlinedSharpIcon from '@mui/icons-material/InsertChartOutlinedSharp';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import SettingsSystemDaydreamOutlinedIcon from '@mui/icons-material/SettingsSystemDaydreamOutlined';
+import AutoAwesomeMotionOutlinedIcon from '@mui/icons-material/AutoAwesomeMotionOutlined';
+import SettingsApplicationsOutlinedIcon from '@mui/icons-material/SettingsApplicationsOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
 import "./sidebar.scss";
 
 const Sidebar = () => {
@@ -9,18 +21,60 @@ const Sidebar = () => {
       <hr />
       <div className="center">
         <ul>
+            <p className="title">MAIN</p>
           <li>
+            <DashboardIcon/>
             <span>Dashboard</span>
           </li>
+            <p className="title">LISTS</p>
           <li>
-            <span>Dashboard</span>
+            <PersonOutlineOutlinedIcon/>
+            <span>Users</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <StoreOutlinedIcon/>
+            <span>Products</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <CreditCardOutlinedIcon/>
+            <span>Orders</span>
           </li>
+          <li>
+            <LocalShippingOutlinedIcon/>
+            <span>Delivery</span>
+          </li>
+            <p className="title">USEFUL LINKS</p>
+          <li>
+            <InsertChartOutlinedSharpIcon/>
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsOutlinedIcon/>
+            <span>Notifications</span>
+          </li>
+            <p className="title">SERVICES</p>
+          <li>
+            <SettingsSystemDaydreamOutlinedIcon/>
+            <span>System Health</span>
+          </li>
+          <li>
+            <AutoAwesomeMotionOutlinedIcon/>
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsApplicationsOutlinedIcon/>
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountCircleOutlinedIcon/>
+            <span>Profile</span>
+          </li>
+          <li>
+            <ExitToAppOutlinedIcon/>
+            <span>Logout</span>
+          </li>
+
         </ul>
       </div>
       <div className="bottom">color options</div>
