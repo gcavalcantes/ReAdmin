91d69b91e22736ab329b42aebc742ce37b5432ca
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2022-10-23 22:32:19 -0300

    Single page Navbar and Sidebar
    Added navbar and sidebar to the single user page.

--- readmin/src/pages/single/Single.jsx
+++ readmin/src/pages/single/Single.jsx
@@ -1,9 +1,15 @@
 import "./single.scss"
-
+import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar"
 
 const Single = () => {
   return (
-    <div>Single</div>
+    <div className="single">
+      <Sidebar/>
+      <div className="singleContainer">
+        <Navbar/>
+      </div>
+    </div>
   )
 }
 
