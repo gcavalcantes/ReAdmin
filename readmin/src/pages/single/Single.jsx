8a56c9cce29c4d3e289e624e49777a9a45c86a26
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2022-10-26 19:41:48 -0300

    Added the product table to the user page
    User page now has the the list of transactions made by the user.

--- readmin/src/pages/single/Single.jsx
+++ readmin/src/pages/single/Single.jsx
@@ -2,6 +2,7 @@ import "./single.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Chart from "../../components/chart/Chart";
+import List from "../../components/table/Table";
 
 const Single = () => {
   return (
@@ -45,7 +46,10 @@ const Single = () => {
             <Chart aspect={3 / 1} title="User Spending ( Last 6 Months )"/>
           </div>
         </div>
-        <div className="bottom"></div>
+        <div className="bottom">
+          <h1 className="title">List Transactions</h1>
+          <List/>
+        </div>
       </div>
     </div>
   );
