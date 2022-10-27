dfa9cf64a7c331d3c2a80a77d1ec6f018a086d31
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2022-10-27 17:04:52 -0300

    Form page design
    Creating the user registration page.

--- readmin/src/pages/new/New.jsx
+++ readmin/src/pages/new/New.jsx
@@ -1,9 +1,61 @@
-import "./new.scss"
+import "./new.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
 
 const New = () => {
   return (
-    <div>New</div>
-  )
-}
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1>Add New User</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img
+              src={
+                "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              }
+              alt=""
+            />
+          </div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label>Username</label>
+                <input type="text" placeholder="john_doe" />
+              </div>
+              <div className="formInput">
+                <label>Name and Surname</label>
+                <input type="text" placeholder="John Doe" />
+              </div>
+              <div className="formInput">
+                <label>Email</label>
+                <input type="email" placeholder="john_doe@gmail.com" />
+              </div>
+              <div className="formInput">
+                <label>Phone</label>
+                <input type="text" placeholder="+1 234 456 678" />
+              </div>
+              <div className="formInput">
+                <label>Password</label>
+                <input type="password"/>
+              </div>
+              <div className="formInput">
+                <label>Address</label>
+                <input type="text" />
+              </div>
+              <div className="formInput">
+                <label>Country</label>
+                <input type="text" placeholder="US" />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default New
+export default New;
