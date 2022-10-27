b14de4873812c913b878cc8a34371ca08b815ff0
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2022-10-27 20:41:12 -0300

    Image display for users and products
    Images now display correctly and when there's no image, a default image appears.

--- readmin/src/pages/new/New.jsx
+++ readmin/src/pages/new/New.jsx
@@ -2,9 +2,12 @@ import "./new.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
-
+import { useState } from "react";
 
 const New = ({ inputs, title }) => {
+  const [file, setFile] = useState("");
+
+  console.log(file)
   return (
     <div className="new">
       <Sidebar />
@@ -16,8 +19,9 @@ const New = ({ inputs, title }) => {
         <div className="bottom">
           <div className="left">
             <img
-              src={
-                "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              src={file 
+                ? URL.createObjectURL(file)
+                : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
               }
               alt=""
             />
@@ -29,7 +33,12 @@ const New = ({ inputs, title }) => {
                   Profile image:{" "}
                   <DriveFolderUploadOutlinedIcon className="icon" />
                 </label>
-                <input type="file" id="file" style={{ display: "none" }} />
+                <input
+                  type="file"
+                  id="file"
+                  onChange={(e) => setFile(e.target.files[0])}
+                  style={{ display: "none" }}
+                />
               </div>
               {inputs.map((input) => (
                 <div className="formInput" key={input.id}>
