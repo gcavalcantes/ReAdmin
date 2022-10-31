b64dd99759b82c77502568209118ca848d7c1151
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2022-10-31 20:26:18 -0300

    Dark mode files and context
    Using context to enable dark mode for the entire website. Dark mode buttons yet to be implemented.

--- readmin/src/App.js
+++ readmin/src/App.js
@@ -6,15 +6,19 @@ import New from "./pages/new/New";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { productInputs, userInputs } from "./formSource";
 import "./style/dark.scss";
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext";
 
 function App() {
+
+  const {darkMode} = useContext(DarkModeContext)
   return (
-    <div className="app dark">
+    <div className={darkMode ? "app dark": "app"}>
       <BrowserRouter>
         <Routes>
           <Route path="/">
             <Route index element={<Home />}></Route>
-            <Route path="login" element={<Login />}></Route>
+            <Route path="login" element={<Login />}> </Route>
             <Route path="users">
               <Route index element={<List />}></Route>
               <Route path=":userId" element={<Single />}></Route>
--- readmin/src/components/datatable/Datatable.jsx
+++ readmin/src/components/datatable/Datatable.jsx
@@ -34,6 +34,7 @@ const Datatable = () => {
         </Link>
       </div>
       <DataGrid
+      className="datagrid"
         rows={userRows}
         columns={userColumns.concat(actionColumn)}
         pageSize={9}
