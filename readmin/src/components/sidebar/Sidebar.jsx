7755150b8b8ccdc400f9b3dfe1d4804c66bbb3bc
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2022-10-31 14:47:27 -0300

    Links for Users and Products
    Created link for the sidebar that goes to the users page and the products page.

--- readmin/src/components/sidebar/Sidebar.jsx
+++ readmin/src/components/sidebar/Sidebar.jsx
@@ -1,80 +1,90 @@
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
-import StoreOutlinedIcon from '@mui/icons-material/StoreOutlined';
-import CreditCardOutlinedIcon from '@mui/icons-material/CreditCardOutlined';
-import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
-import InsertChartOutlinedSharpIcon from '@mui/icons-material/InsertChartOutlinedSharp';
-import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
-import SettingsSystemDaydreamOutlinedIcon from '@mui/icons-material/SettingsSystemDaydreamOutlined';
-import AutoAwesomeMotionOutlinedIcon from '@mui/icons-material/AutoAwesomeMotionOutlined';
-import SettingsApplicationsOutlinedIcon from '@mui/icons-material/SettingsApplicationsOutlined';
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
-import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import StoreOutlinedIcon from "@mui/icons-material/StoreOutlined";
+import CreditCardOutlinedIcon from "@mui/icons-material/CreditCardOutlined";
+import LocalShippingOutlinedIcon from "@mui/icons-material/LocalShippingOutlined";
+import InsertChartOutlinedSharpIcon from "@mui/icons-material/InsertChartOutlinedSharp";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
+import AutoAwesomeMotionOutlinedIcon from "@mui/icons-material/AutoAwesomeMotionOutlined";
+import SettingsApplicationsOutlinedIcon from "@mui/icons-material/SettingsApplicationsOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import ExitToAppOutlinedIcon from "@mui/icons-material/ExitToAppOutlined";
 import "./sidebar.scss";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="top">
-        <span className="logo">readmin</span>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">readmin</span>
+        </Link>
       </div>
       <hr />
       <div className="center">
         <ul>
-            <p className="title">MAIN</p>
+          <p className="title">MAIN</p>
           <li>
-            <DashboardIcon className='icon'/>
+            <DashboardIcon className="icon" />
             <span>Dashboard</span>
           </li>
-            <p className="title">LISTS</p>
+          <p className="title">LISTS</p>
+          <Link to="/users" style={{ textDecoration: "none" }}>
+            <li>
+              <PersonOutlineOutlinedIcon className="icon" />
+              <span>Users</span>
+            </li>
+          </Link>
+          <Link to="/products" style={{ textDecoration: "none" }}>
+            <li>
+              <StoreOutlinedIcon className="icon" />
+              <span>Products</span>
+            </li>
+          </Link>
+          <Link to="/orders" style={{ textDecoration: "none" }}>
+            <li>
+              <CreditCardOutlinedIcon className="icon" />
+              <span>Orders</span>
+            </li>
+          </Link>
+          <Link to="/delivery" style={{ textDecoration: "none" }}>
+            <li>
+              <LocalShippingOutlinedIcon className="icon" />
+              <span>Delivery</span>
+            </li>
+          </Link>
+          <p className="title">USEFUL LINKS</p>
           <li>
-            <PersonOutlineOutlinedIcon className='icon'/>
-            <span>Users</span>
-          </li>
-          <li>
-            <StoreOutlinedIcon className='icon'/>
-            <span>Products</span>
-          </li>
-          <li>
-            <CreditCardOutlinedIcon className='icon'/>
-            <span>Orders</span>
-          </li>
-          <li>
-            <LocalShippingOutlinedIcon className='icon'/>
-            <span>Delivery</span>
-          </li>
-            <p className="title">USEFUL LINKS</p>
-          <li>
-            <InsertChartOutlinedSharpIcon className='icon'/>
+            <InsertChartOutlinedSharpIcon className="icon" />
             <span>Stats</span>
           </li>
           <li>
-            <NotificationsOutlinedIcon className='icon'/>
+            <NotificationsOutlinedIcon className="icon" />
             <span>Notifications</span>
           </li>
-            <p className="title">SERVICES</p>
+          <p className="title">SERVICES</p>
           <li>
-            <SettingsSystemDaydreamOutlinedIcon className='icon'/>
+            <SettingsSystemDaydreamOutlinedIcon className="icon" />
             <span>System Health</span>
           </li>
           <li>
-            <AutoAwesomeMotionOutlinedIcon className='icon'/>
+            <AutoAwesomeMotionOutlinedIcon className="icon" />
             <span>Logs</span>
           </li>
           <li>
-            <SettingsApplicationsOutlinedIcon className='icon'/>
+            <SettingsApplicationsOutlinedIcon className="icon" />
             <span>Settings</span>
           </li>
           <p className="title">USER</p>
           <li>
-            <AccountCircleOutlinedIcon className='icon'/>
+            <AccountCircleOutlinedIcon className="icon" />
             <span>Profile</span>
           </li>
           <li>
-            <ExitToAppOutlinedIcon className='icon'/>
+            <ExitToAppOutlinedIcon className="icon" />
             <span>Logout</span>
           </li>
-
         </ul>
       </div>
       <div className="bottom">
